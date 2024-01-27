936e32426c1e9b755ac3e0c037537025f0af8aac
Justin Johnson <jwj019@gmail.com>
2024-01-27 16:52:59 -0500

    Update palServer.ps1

--- palServer.ps1
+++ palServer.ps1
@@ -1,5 +1,41 @@
 # Define the path to PalServer.exe
-$palServerExe = "C:\Program Files (x86)\Steam\steamapps\common\PalServer\PalServer.exe" # Update this path
+$palServerPath = "C:\Program Files (x86)\Steam\steamapps\common\PalServer"
+$palServerExe = "$palServerPath\PalServer.exe" # Update this path
+$defaultSteamCmdPath = "C:\SteamCMD" # Default steamcmd installation path
+
+# Function to download and unzip steamcmd if not installed
+function Install-SteamCmd {
+    $steamCmdZipUrl = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+    $steamCmdZipPath = "$defaultSteamCmdPath\steamcmd.zip"
+    $webClient = New-Object System.Net.WebClient
+    
+    # Create the directory if it doesn't exist
+    if (-not (Test-Path $defaultSteamCmdPath)) {
+        New-Item -Path $defaultSteamCmdPath -ItemType Directory
+    }
+
+    # Download and unzip steamcmd
+    $webClient.DownloadFile($steamCmdZipUrl, $steamCmdZipPath)
+    Expand-Archive -Path $steamCmdZipPath -DestinationPath $defaultSteamCmdPath -Force
+    Remove-Item -Path $steamCmdZipPath # Clean up ZIP file after extraction
+}
+
+# Function to check if steamcmd is installed
+function Get-SteamCmdInstalled {
+    $isInPath = Get-Command steamcmd -ErrorAction SilentlyContinue
+    $existsInDefaultLocation = Test-Path "$defaultSteamCmdPath\steamcmd.exe"
+
+    if ($isInPath) {
+        Write-Host "steamcmd is installed."
+    } else {
+        if (-not $existsInDefaultLocation) {
+            Write-Host "steamcmd not found. Installing..."
+            Install-SteamCmd
+            # Add the default location to the PATH for this session
+            $env:Path += ";$defaultSteamCmdPath"
+        }
+    }
+}
 
 # Function to check and update the game server
 function Update-GameServer {
@@ -9,7 +45,8 @@ function Update-GameServer {
 
 # Function to start the game server
 function Start-GameServer {
-    Start-Process $palServerExe 
+    $arguments = "-log -nosteam -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
+    Start-Process -FilePath $palServerExe -ArgumentList $arguments -NoNewWindow
 }
 
 # Function to check if the server is running
@@ -23,18 +60,44 @@ function Get-Timestamp {
     return Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 }
 
-# Update game server
-Update-GameServer
+function Backup-ServerData {
+    $sourcePath = "$palServerPath\Pal\Saved\"
+    $backupParentPath = "$palServerPath\Pal\Backups"
+    $backupPath = "$backupParentPath\Saved-Backup-" + (Get-Date -Format "yyyyMMddHHmmss")
+    Write-Host "Backing up server data to $backupPath..."
+    # Create the directory if it doesn't exist
+    if (-not (Test-Path $backupParentPath)) {
+        New-Item -Path $backupParentPath -ItemType Directory
+    }
+    # Create the directory if it doesn't exist
+    Copy-Item -Path $sourcePath -Destination $backupPath -Recurse -Force
+}
 
-# Start game server
+# Initial setup and server start
+Get-SteamCmdInstalled
+Update-GameServer
 Start-GameServer
+$timestamp = Get-Timestamp
+Write-Host "$timestamp - Started PalServer.exe!"
 
 # Continuously check if the server is running
+$backupInterval = 15 * 60 # 15 minutes in seconds
+$checkInterval = 10 # Check every 10 seconds
+$backupTimer = 0
 while ($true) {
-    Start-Sleep -Seconds 10 # Wait for 10 seconds before checking the process
+    Start-Sleep -Seconds $checkInterval
+    $backupTimer += $checkInterval
 
+    # Check server status
     if (-not (Get-ServerRunning)) {
+        $timestamp = Get-Timestamp
         Write-Host "$timestamp - PalServer.exe is not running. Starting server..."
         Start-GameServer
+    }
+
+    # Every 15 minutes, backup server data
+    if ($backupTimer -ge $backupInterval) {
+        Backup-ServerData
+        $backupTimer = 0 # Reset timer after backup
     }
 }
